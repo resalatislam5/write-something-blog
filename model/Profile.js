5f58e4bb247e6fd9499c5a2753ff87f4c20ca335
Resalat islam <resalatislam5@gmail.com>
2023-09-22 18:23:43 +0600

    create a post model

---
+++ model/Profile.js
@@ -0,0 +1,43 @@
+const {Schema, model} = require('mongoose');
+
+const profileSchema = new Schema({
+    user:{
+        type : Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title:{
+        type: String,
+        trim: true,
+        maxlangth: 100
+    },
+    bio:{
+        type: String,
+        trim: true,
+        maxlangth: 500
+    },
+    avater: String,
+    links:{
+        website: String,
+        facebook: String,
+        twiter: String,
+        github: String
+    },
+    post: [
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Post'
+        }
+    ],
+    bookmark:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        } 
+    ]
+},{
+    timestamps: true
+})
+
+const Profile = model('Profile', profileSchema)
+module.exports = Profile
